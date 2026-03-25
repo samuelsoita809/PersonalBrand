387683a54b8f9d37f916e7ea68a3eb82e491314a
samuelsoita809 <samuelsoita809@gmail.com>
2026-03-25 13:39:42 +0300

    chore: merge CTA cleanup (Slices 3-6) into development

--- apps/frontend/src/components/registry.ts
+++ apps/frontend/src/components/registry.ts
@@ -4,7 +4,6 @@ import HeroSection from './HeroSection';
 
 export const componentRegistry: Record<string, React.LazyExoticComponent<any> | React.FC<any>> = {
   Card: lazy(() => import('./Card')),
-  Modal: lazy(() => import('./Modal')),
   InfoPanel: lazy(() => import('./InfoPanel')),
   ContactForm: lazy(() => import('./ContactForm')),
   HeroSection,
